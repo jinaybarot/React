57c434c20f2099516ccc4b69486ae21be80b1a7d
Barot Jinay <143944654+jinaybarot@users.noreply.github.com>
2024-09-18 19:43:47 +0530

    Refactor password generation logic and add copy to clipboard functionality

--- PasswGenerator/src/App.jsx
+++ PasswGenerator/src/App.jsx
@@ -1,60 +1,92 @@
-import { StrictMode } from 'react'
-import { useState, useCallback } from 'react'
+
+import { useState, useCallback, useEffect, useRef } from 'react'
 
 
 function App() {
-  const [length, setLenght] = useState(8)
+  const [length, setLength] = useState(8)
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState('')
 
-  const PasswordGenerator = () => useCallback(() => {
+  const passwordRef = useRef(null)
+
+  const passwordGenerator = useCallback(() => {
     let pass = ""
-    let string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
-    if (numberAllowed) string += "0123456789"
-    if (charAllowed) string += " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+    if (numberAllowed) str += "0123456789"
+    if (charAllowed) str += " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
 
-    for (let i = 1; i < Array.length; i++) {
-     let char = Math.floor(Math.random() * string.length + 1)
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
 
-     pass = str.charAt(char)
+      pass += str.charAt(char)
     }
 
-      setPassword(pass)
+    setPassword(pass)
 
   }, [length, numberAllowed, charAllowed, setPassword])
 
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 999);
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+
+  useEffect(() => {
+    passwordGenerator() }, [length, numberAllowed, charAllowed, passwordGenerator]) 
+ 
   return (
-    <>
+   
       <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-800'>
         <h1 className='text-2xl text-center font-bold py-2'>Password Generator</h1>
         <div className='flex shadow rounded-lg overflow-hidden mb-4'>
-              <input 
-              type='text'
-              value = {password}
-              className= 'outline-none w-full py-1 px-3'
-              placeholder = 'Password'  
-              readOnly
-              />
-              <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>copy</button>
+          <input
+            type='text'
+            value={password}
+            className='outline-none w-full py-1 px-3'
+            placeholder='Password'
+            readOnly
+            ref={passwordRef} 
+          />
+          <button
+          onClick={copyPasswordToClipboard}
+          className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>copy</button>
 
         </div>
-        <div className ='flex text-sm gap-x-2'>
+        <div className='flex text-sm gap-x-2'>
           <div className='flex items-center gap-x-1'>
-            <input 
-            type="range" 
-            min={6} 
-            max={100}
-            value={length}
-            className='cursor-pointer'
-            onChange={(e) => setLenght(e.target.value)}
+            <input
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className='cursor-pointer'
+              onChange={(e) => setLength(e.target.value)}
             />
             <label> Length: {length}</label>
           </div>
+          <div className='flex items-center gap-x-1'>
+          <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="numberInput"
+              onChange={() =>{ setNumberAllowed((prev) => !prev)}}
+            />
+            <label htmlFor="numberInput">Numbers</label>
+            </div>
+            <div className='flex items-center gap-x-1'>
+          <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="characterrInput"
+              onChange={() =>{setCharAllowed((prev) => !prev)}}
+            />
+            <label htmlFor="characterInput">Character</label>
+            </div>
         </div>
       </div>
-    </>
+    
   )
 }
 
